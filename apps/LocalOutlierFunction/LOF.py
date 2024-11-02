ecb62e855d570234c0eeab046945e864ae288e50
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-11-03 01:12:58 +0800

    Update LOF.py

--- apps/LocalOutlierFunction/LOF.py
+++ apps/LocalOutlierFunction/LOF.py
@@ -7,7 +7,7 @@ from skimage import exposure
 from PIL import Image
 
 # Load the KNN model using pickle
-model_path = 'apps/OC-KNN/one_class_knn_model (1).pkl'
+model_path = 'apps/LocalOutlierFunction/lof_model.pkl'
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
 
