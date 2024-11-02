e0d13bc81632432e7db22896f279c98d20617649
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-11-03 02:40:42 +0800

    Update LOF.py

--- apps/LocalOutlierFunction/LOF.py
+++ apps/LocalOutlierFunction/LOF.py
@@ -28,8 +28,10 @@ def process_and_predict(img, model):
     image = np.array(img)
     if image is not None:
         features = extract_hog_features(image).reshape(1, -1)  # Extract HOG features
-        prediction = model.predict(features)  # Use the LOF model to predict
-
+        
+        # Use the LOF model to predict
+        prediction = model.fit_predict(features)  # Fit the model on the features to predict
+        
         # Convert prediction to readable format
         if prediction[0] == 1:
             return "rebar"
