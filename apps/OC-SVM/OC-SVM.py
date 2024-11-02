851639dbf17a9a2be9a0d654943da6208fc2066b
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-11-02 09:32:17 +0800

    Update OC-SVM.py

--- apps/OC-SVM/OC-SVM.py
+++ apps/OC-SVM/OC-SVM.py
@@ -9,7 +9,7 @@ from sklearn.svm import OneClassSVM
 from PIL import Image
 
 # Load the model using pickle
-model_path = '/mount/src/rebar-detector/apps/OC_svm_model.pkl'  # Ensure this path is correct
+model_path = 'apps/OC-SVM/OC_svm_model.pkl'  # Ensure this path is correct
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
 
